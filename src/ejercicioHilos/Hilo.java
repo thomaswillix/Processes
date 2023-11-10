487c15677da913717a8a06ce892e40a4803202a0
Thomas <117021963+thomaswillix@users.noreply.github.com>
2023-11-10 10:14:06 +0100

    Update Hilo.java

--- src/ejercicioHilos/Hilo.java
+++ src/ejercicioHilos/Hilo.java
@@ -8,24 +8,22 @@ public class Hilo extends Thread {
 	Scanner sc = new Scanner(System.in);
 	int num;
 
-
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
 		System.out.println("¿En qué año se empezó a publicar One Piece?"
-				+ "\nSi respondes mal, te eliminaré todos los datos del disco duro.");
-		do {
-			num = sc.nextInt();
-			comprobar(num);
-		} while (!(num == res));
-		System.out.println("Respuesta correcta :D");
-		System.exit(0);
+				+ "\nSi respondes mal, te eliminaré todos los datos del disco duro.");	
+		num = sc.nextInt();
+		comprobar(num);	
 	}
 
 	public void comprobar(int num) {
-		if (num != res) {
+		while (num != res) {
 			System.out.println("Ese no es el año correcto, dame otro");
-		}
+			num = sc.nextInt();
+		} 
+		System.out.println("Respuesta correcta :D");
+		System.exit(0);
 	}
 
 	public void interrumpir() {
