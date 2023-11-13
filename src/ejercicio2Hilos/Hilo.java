3b6238ef812ab56cc3b33316a1ba5ceecdcd1b57
Thomas <thomaswfds@gmail.com>
2023-11-13 22:57:56 +0100

    11/11/2023

--- src/ejercicio2Hilos/Hilo.java
+++ src/ejercicio2Hilos/Hilo.java
@@ -1,5 +1,33 @@
 package ejercicio2Hilos;
 
-public class Hilo extends Thread{
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 
+public class Hilo extends Thread{
+    char[] buffer;
+    char[] vocales = {'a','e','i','o','u'};
+    Scanner sc =  new Scanner(System.in);
+
+
+    @Override
+    public void run() {
+        //leerArchivoVocal();
+    }
+    /*private void leerArchivoVocal(){
+        try {
+            File f = new File(ejemplo.txt);
+            FileReader fr =  new FileReader(f);
+            while(true) {
+                fr.read(buffer);
+
+            }
+        }catch (IOException e){
+            System.err.println("Error de lectura y/o escritura");
+        } catch(FileNotFoundException e){
+            System.err.println("Archivo no encontrado");
+        }
+    }*/
 }
---
+++ src/ejercicio2Hilos/Main.java
@@ -0,0 +1,8 @@
+package ejercicio2Hilos;
+
+public class Main {
+    public static void main(String[] args) {
+        Hilo h =  new Hilo();
+        h.start();
+    }
+}
