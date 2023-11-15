01bb3d1dd441ef36ad0072c028f6358d3d327c7d
Thomas <117021963+thomaswillix@users.noreply.github.com>
2023-11-15 12:50:25 +0100

    Update Main.java

--- src/ejercicio2Hilos/Main.java
+++ src/ejercicio2Hilos/Main.java
@@ -1,24 +1,16 @@
 package ejercicio2Hilos;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 
 public class Main {
-    public static void main(String[] args) {
-        
-        char[] vocales = {'a','e','i','o','u'};
-        for (int i = 0; i < vocales.length; i++) {
-        	Hilo h =  new Hilo();
-            h.run(vocales[i]);
-            
-            /*BufferedWriter bw = new BufferedWriter(new FileWriter(f));
-            for (char clave : vocales[i]) {
-                int valor = veces.get(clave);
-                frase = "La letra: " + clave + ", sale " + valor + "\n";
-                bw.write(frase);
-            }
-            bw.close();*/
+	public static void main(String[] args) {
+		char[] vocales = {'a','e','i','o','u'};
+		
+		
+		for (int i = 0; i < vocales.length; i++) {
+			Hilo h = new Hilo(vocales[i]);
+			h.start();
+
 		}
-        
-    }
+		
+	}
 }
