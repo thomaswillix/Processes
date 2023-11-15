83467dafde1b1f480e8ffab795d2bf770862e8fb
Thomas <117021963+thomaswillix@users.noreply.github.com>
2023-11-15 12:49:54 +0100

    Update Hilo.java

--- src/ejercicio2Hilos/Hilo.java
+++ src/ejercicio2Hilos/Hilo.java
@@ -4,24 +4,44 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class Hilo extends Thread{
-    int cont ;
+public class Hilo extends Thread {
+	int cont;
+	char voc;
+	
+	public Hilo(char i) {
+		this.voc = i;
+	}
+	
+	@Override
+	public void run() {
+		try {
+			sleep(100);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Entramos al bucle cuentaVocales " + voc);
+		cuentaVocal(voc);	
 
-    public void run(char l) {
-    	 try {
-             File f = new File("ejemplo.txt");
-             FileReader fr =  new FileReader(f);
-             int c = fr.read();
-             while(c != -1) {
-             	if ((char)c == l) {
- 					cont++;
- 					c = fr.read();
- 				}                 
-             }
-             fr.close();
-             System.out.println("la vocsl " + l + " aparece " +cont + " veces");
-         }catch (IOException e){
-             System.err.println("Error de lectura y/o escritura");
-         } 
-    }
+	}
+	
+	public void cuentaVocal(char l) {
+		try {
+			File f = new File("ejemplo.txt");
+			
+			FileReader fr = new FileReader(f);
+			int c = fr.read();
+			
+			while (c != -1) {
+				if ((char) c == l) {
+					cont++;
+				}
+				c = fr.read();
+			}
+			
+			fr.close();
+			System.out.println("la vocal " + l + " aparece " + cont + " veces");
+			cont = 0;
+		} catch (IOException e) {
+		}
+	}
 }
