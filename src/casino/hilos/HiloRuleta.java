a67fa6852a60b43aefe44b88307bac8087f331bb
Thomas <117021963+thomaswillix@users.noreply.github.com>
2023-11-30 10:25:35 +0100

    Update HiloRuleta.java

--- src/casino/hilos/HiloRuleta.java
+++ src/casino/hilos/HiloRuleta.java
@@ -6,6 +6,7 @@ public class HiloRuleta extends Thread {
 
 	private Ruleta bola;
 	private boolean tienePasta = true;
+	
 	public HiloRuleta(Ruleta b) {
 		this.bola = b;
 	}
@@ -14,7 +15,7 @@ public class HiloRuleta extends Thread {
 	public void run() {
 		int aux = 0;
 		int aux1 = 0;
-		while (tienePasta && !(aux == bola.getBanca() && aux1 != bola.getNum())) {
+		while (tienePasta || !(aux == bola.getBanca() && aux1 != bola.getNum())) {
 			try {
 				sleep(3000);
 				aux1 = bola.getNum();
@@ -28,13 +29,16 @@ public class HiloRuleta extends Thread {
 			}			
 			System.out.println("numero: "+bola.getNum());
 			System.out.println("Banca " + bola.getBanca());
+			
 			aux =  bola.getBanca();	
+			
 			if (bola.getBanca()<=0) {
 				tienePasta = false;
 			}
-		}
-		System.out.println("Banca " + bola.getBanca());
+			System.out.println("Banca " + bola.getBanca());
 
+		}
+		
 		interrupt();
 	}
 }
