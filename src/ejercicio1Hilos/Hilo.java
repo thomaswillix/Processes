c05f6db686214e67f8aa899f29635eb73c37a351
Thomas <117021963+thomaswillix@users.noreply.github.com>
2024-01-18 21:10:58 +0100

    Update Hilo.java

--- src/ejercicio1Hilos/Hilo.java
+++ src/ejercicio1Hilos/Hilo.java
@@ -2,32 +2,38 @@ package ejercicio1Hilos;
 
 import java.util.Scanner;
 
-public class Hilo extends Thread {
-
-	private int res = 1997;
-	Scanner sc = new Scanner(System.in);
-	int num;
-
+public class Hilo extends Thread{
+	
+	private int res =  1997;
+	Scanner sc =  new Scanner(System.in);
+	
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
 		System.out.println("¿En qué año se empezó a publicar One Piece?"
 				+ "\nSi respondes mal, te eliminaré todos los datos del disco duro.");
-		num = sc.nextInt();
-		comprobar(num);
-	}
-
-	public void comprobar(int num) {
-		while (num != res) {
-			System.out.println("Ese no es el año correcto, dame otro");
-			num = sc.nextInt();
+		int num =0;
+		try {
+			Thread.sleep(5000);
+			while (!isInterrupted()) {
+				comprobar(num);
+				System.out.println("Respuesta correcta :D");
+			}
+		}catch (InterruptedException e) {
+            System.err.println("Borrando todos los datos de tu disco...");
 		}
-		System.out.println("Respuesta correcta :D");
-		System.exit(0);
+	}
+	
+	public void comprobar(int num) {
+		num = sc.nextInt();
+		while (num!=res) {
+			System.out.println("Ese no es el año correcto, dame otro");
+			num= sc.nextInt();
+		}
+		interrumpir();
 	}
 
 	public void interrumpir() {
-		System.err.println("Borrando todos los datos de tu disco...");
-		interrupt();
+        interrupt();
 	}
 }
