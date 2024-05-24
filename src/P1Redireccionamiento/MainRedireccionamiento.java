99440c59af3289a04107f89a1d2dbebb7c8cd773
thomaswillix <thomaswfds@gmail.com>
2024-05-24 19:11:09 +0200

    renamed stuff

---
+++ src/P1Redireccionamiento/MainRedireccionamiento.java
@@ -0,0 +1,26 @@
+package P1Redireccionamiento;
+import java.io.File;
+import java.io.IOException;
+
+public class MainRedireccionamiento {
+	public static void main(String[] args) {
+		ProcessBuilder pb= new ProcessBuilder("./juego");
+		File fin = new File("entrada.txt");
+		File fout = new File("salida.txt");
+		File ferr =  new File("error.txt");
+		
+		pb.redirectInput(fin);
+		pb.redirectOutput(fout);
+		pb.redirectError(ferr);
+		
+		System.out.println("lanzando un comando");
+		
+		try {
+			pb.start();
+		}
+		catch (IOException e)
+		{
+ 			e.printStackTrace();
+ 		}
+	}
+}
