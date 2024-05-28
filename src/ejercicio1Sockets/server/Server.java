89ee354400193e9f91372e75799ac1dd8d3dd7b6
thomaswillix <thomaswfds@gmail.com>
2024-05-28 21:52:34 +0200

    Ejercicio de cliente-servidor Dummy chat en el que el servidor contesta con mensajes aleatorios y sin sentido al cliente. Se acaba mediante un comando

--- src/ejercicio1Sockets/server/Server.java
+++ src/ejercicio1Sockets/server/Server.java
@@ -4,27 +4,56 @@ package ejercicio1Sockets.server;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class Server {
 
-    private static ServerSocket ssocket;
-    private static Socket cliente;
+    private static List<String> mensajesAmbiguos;
     public static void main(String[] args){
+        cargarMensajes();
         try {
-            ssocket = new ServerSocket(1234);
+            ServerSocket ssocket = new ServerSocket(12345);
+            Socket cliente = ssocket.accept();
+            System.out.println("connection accepted");
 
-            while (true) {
-                cliente = ssocket.accept();
-                System.out.println("conexión aceptada");
-                DataInputStream in = new DataInputStream(cliente.getInputStream());
-                String dataReceived = in.readUTF();
+            DataInputStream in = new DataInputStream(cliente.getInputStream());
+            DataOutputStream out = new DataOutputStream(cliente.getOutputStream());
+
+            String dataReceived;
+            while (!(dataReceived = in.readUTF()).equals("adios")) {
                 System.out.println("Server received: " + dataReceived);
-                DataOutputStream out = new DataOutputStream(cliente.getOutputStream());
-                out.writeUTF("Hola, " + dataReceived+"\n");
-                cliente.close();
+                out.writeUTF(escribirMensajeAmbiguo());
             }
+            out.writeUTF("/salir");
+            cliente.close();
         }catch (IOException e){
             e.printStackTrace();
         }
+    }
+    private static void cargarMensajes(){
+        File f = new File("files/frases.txt");
+        String line;
+        mensajesAmbiguos = new ArrayList<>();
+        try {
+            BufferedReader bf = new BufferedReader(new FileReader(f));
+            try {
+                while ((line = bf.readLine())!=null){
+                    mensajesAmbiguos.add(line);
+                }
+            } catch (EOFException e) {
+                System.err.println("End of file");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found");
+        }
+    }
+    private static String escribirMensajeAmbiguo(){
+        Random r = new Random();
+        int n = r.nextInt(mensajesAmbiguos.size());
+        return mensajesAmbiguos.get(n);
     }
 }
