e94b019d03644424d26082c3b3f178fc0eafc666
thomaswillix <thomaswfds@gmail.com>
2024-05-28 21:52:34 +0200

    Ejercicio de cliente-servidor Dummy chat en el que el servidor contesta con mensajes aleatorios y sin sentido al cliente. Se acaba mediante un comando

--- src/ejercicio1Sockets/client/Client.java
+++ src/ejercicio1Sockets/client/Client.java
@@ -7,21 +7,15 @@ import java.util.Scanner;
 public class Client {
     private static Socket socket;
     public static void main(String[] args) {
+        System.out.println("----------------------Chat con IA----------------------\n" +
+                "\n----------------------COMANDOS----------------------\nadios --> Salir" +
+                " del chat\n------------------------------------------------------");
+
         try {
-            socket = new Socket("127.0.0.1", 1234);
-
-            String nombre;
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Escribe tu nombre: ");
-            nombre = sc.nextLine();
-
-            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-            out.writeUTF(nombre);
-
-            DataInputStream in = new DataInputStream(socket.getInputStream());
-            String respuesta = in.readUTF();
-            System.out.println(respuesta);
-            socket.close();
+            socket = new Socket("127.0.0.1", 12345);
+            ClientHandler handler = new ClientHandler(socket);
+            Thread t = new Thread(handler);
+            t.start();
         } catch (IOException e){
             e.printStackTrace();
         }
